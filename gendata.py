a97846397454db759d251c0a922a3146311a2196
Shinya Okano <shinya.okano@open-c.jp>
2024-05-16 02:58:05 +0900

    orjson

--- gendata.py
+++ gendata.py
@@ -1,10 +1,13 @@
 import random
-import json
+import orjson
 from uuid import uuid4
+
+DATA_FILE = "data.jsonl"
 
 nums = list(range(1, 10000001))
 random.shuffle(nums)
 
-for num in nums:
-    line = json.dumps({"id": num, "value": str(uuid4().hex)})
-    print(line)
+with open(DATA_FILE, "wb") as output_file:
+    for num in nums:
+        line = orjson.dumps({"id": num, "value": str(uuid4().hex)})
+        output_file.write(line + b"\n")
