25f5cfc5a3c3d6a0c8bd7c5ea37d5eac9bf2d857
Shinya Okano <shinya.okano@open-c.jp>
2024-05-21 03:08:28 +0900

    webapp

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 ```
 gunicorn --threads=10 -b=127.0.0.1:5000 httpbin:app
 ```
+
+## flask appの起動
+
+```
+flask --app webapp:app run -p 8000
+```
---
+++ webapp.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from call_api import main as call_api_main
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    import cProfile
+    with cProfile.Profile() as pr:
+        # プロファイル対象のコードここから
+        call_api_main()
+        # プロファイル対象のコードここまで
+        pr.dump_stats("webapp.profile")
+    return "Finished"
